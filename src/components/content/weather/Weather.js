b408d114e6b1b26b149d40e6aab2911dad9cb9cf
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-31 23:55:16 +0200

    moved function to fatchData.js

--- src/components/content/weather/Weather.js
+++ src/components/content/weather/Weather.js
@@ -3,99 +3,63 @@ import "./Weather.css";
 import SearchBox from "./searcbox/SearchBox";
 import CardList from "./cardlist/CardList";
 import CityWeather from "./CityWeather/CityWeather";
-import logo from "../../../assets/images/logo.png";
+// import logo from "../../../assets/images/logo.png";
+import { getCurrentWeather, getDailyForecasts, getCityKey } from "./fetchData";
+import { Grid } from "@material-ui/core";
 
-const API_KEY = "oAANJ78p2PzQFD4707OrKAwj5DPuPbQZ";
+const API_KEY = "dRfaOkfZ2jp11Clgg6cn7m1i11WLTZEL";
 const DEFAULT_CITY_KEY = "215854";
 
-const dayList = [
-  { id: 1, name: "mondey", tempMin: "1", tempMax: "1" },
-  { id: 1, name: "sunday", tempMin: "1", tempMax: "1" },
-  { id: 1, name: "sunday", tempMin: "1", tempMax: "1" }
-];
-
-const getWeekDayName = date => {
-  const weekday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  return weekday[date.getDay()];
-};
+// const dayList = [
+//   { id: 1, name: "mondey", tempMin: "1", tempMax: "1" },
+//   { id: 1, name: "sunday", tempMin: "1", tempMax: "1" },
+//   { id: 1, name: "sunday", tempMin: "1", tempMax: "1" }
+// ];
 
 const Weather = () => {
   const [searchfield, setSearchfield] = useState("");
   const [cityKey, setCityKey] = useState(DEFAULT_CITY_KEY);
-  const [days, setDays] = useState(dayList);
+  const [days, setDays] = useState([]);
   const [cityData, setCityData] = useState([]);
+  const [cityName, setCityName] = useState("Tel-Aviv");
   const [isMetric, setIsMetric] = useState(true);
-  // const [forecasts, setForecasts] = useState([]);
-
-  async function getCurrentWeather() {
-    const res = await fetch(
-      `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}.json?apikey=${API_KEY}`
-    );
-    res
-      .json()
-      .then(res => {
-        setCityData(res[0]);
-      })
-      .catch(err => console.log);
-  }
-  async function getDailyForecasts() {
-    const res = await fetch(
-      `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}.json?apikey=${API_KEY}`
-    );
-    res
-      .json()
-      .then(res => {
-        forcatsToDays(res);
-      })
-      .catch(err => console.log);
-  }
 
   useEffect(() => {
-    getCurrentWeather();
-    getDailyForecasts();
-  }, []);
+    getCurrentWeather(API_KEY, setCityData, cityKey);
+    getDailyForecasts(API_KEY, setDays, cityKey);
+    getCityKey(API_KEY, setCityKey);
+  }, [cityKey]);
 
   const onSearchChange = event => {
     setSearchfield(event.target.value);
   };
 
-  const forcatsToDays = data => {
-    if (data.DailyForecasts) {
-      const dailyForecasts = data.DailyForecasts;
-      let myList = [];
-      for (let i = 0; i < dailyForecasts.length; i++) {
-        let dayName = new Date(dailyForecasts[i].Date);
-        myList.push({
-          id: i,
-          image: logo,
-          name: getWeekDayName(dayName),
-          tempMin: dailyForecasts[i].Temperature.Minimum.Value,
-          tempMax: dailyForecasts[i].Temperature.Maximum.Value
-        });
-      }
-      setDays(myList);
-    }
-  };
-
   return (
     <div className="weather">
       <div className="weather-panel shadow-5">
-        <div className="tc">
-          <div className="weather-title">
-            <h1>Weather</h1>
-          </div>
-          <SearchBox searchChange={onSearchChange} />
-          <CityWeather data={cityData} isMetric={isMetric} />
-          <CardList days={days} />
-        </div>
+        <Grid container>
+          {/* <div className="header shadow-5"> */}
+          <Grid item xs={12} className="header shadow-5">
+            <Grid item xs={12}>
+              <div className="weather-title">
+                <h1>My Weather</h1>
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <SearchBox searchChange={onSearchChange} />
+            </Grid>
+          </Grid>
+          <Grid item lg={3} xs={12}>
+            <CityWeather
+              data={cityData}
+              cityName={cityName}
+              isMetric={isMetric}
+            />
+          </Grid>
+          <Grid item lg={9} xs={12}>
+            <CardList days={days} />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
