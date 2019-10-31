0084ff4d2668545e77d5dd916e1d9269bf39edb9
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-31 22:40:31 +0200

    display metric or imperial accourding to props

--- src/components/content/weather/CityWeather/CityWeather.js
+++ src/components/content/weather/CityWeather/CityWeather.js
@@ -6,16 +6,20 @@ const CityWeather = ({ data, isMetric }) => {
   const unit = isMetric ? "°C" : "°F";
   let information = "";
   if (data.Temperature) {
+    const tempVal = isMetric
+      ? data.Temperature.Metric.Value
+      : data.Temperature.Imperial.Value;
     information = (
       <div>
         <h2>{data.WeatherText}</h2>
         <img alt="weather" src={logo} />
-        <h2>{data.Temperature.Metric.Value}</h2>
-        <h2>{unit}</h2>
+        <h2>
+          {tempVal} {unit}
+        </h2>
       </div>
     );
   }
-  return <div className="city-weather shadow-5">{information}</div>;
+  return <div className="city-weather shadow-5 white">{information}</div>;
 };
 
 export default CityWeather;
