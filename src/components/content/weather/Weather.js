5bd819f51f435fe0ec29defa498f628c3f7c0177
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-31 14:55:09 +0200

    added place holder searchbar and cards

--- src/components/content/weather/Weather.js
+++ src/components/content/weather/Weather.js
@@ -1,39 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Weather.css";
+import SearchBox from "./searcbox/SearchBox";
+import CardList from "./cardlist/CardList";
 
 const Weather = () => {
+  const [searchfield, setSearchfield] = useState("");
+  const [robots, setRobots] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(respone => respone.json())
+      .then(users => setRobots(users))
+      .catch(console.log);
+  }, []);
+
+  const onSearchChange = event => {
+    setSearchfield(event.target.value);
+  };
+
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
   return (
     <div className="weather">
       <div className="weather-panel shadow-5">
-        <h1 className="weather-title">Weather</h1>
-        <div>
-          <p>
-            I was first exposed to programming at 8th grade when my friend
-            showed me a c++ program he wrote, which seemed very cool to me.
-            Together we wrote very simple text games, using tutorials in
-            Youtube.
-          </p>
-          <p>
-            In 10th grade, I choose to study computer science and physics. In
-            the computer science class, we learned the basics of programming in
-            C#. I also made a simple news site using HTML/CSS/JS and C# with
-            .NET framework. I also participated in a project called "Computer
-            Science, Academia and Industry" by Weizmann Institute of Science, in
-            which I made a simple geometric optics simulator (WinForms
-            application) using C#.
-          </p>
-          <p>
-            In the middle of my mandatory army service, I decided I miss
-            studying and I was motivated to develop myself. To so I started to
-            take Computer Science courses at the Open University. I studies
-            fully independently using mostly books and some videos at the
-            University site.
-          </p>
-          <p>
-            Today I study independently second-year courses but interested in my
-            first job in the field. In the meanwhile, I also study using the
-            internet Python, React, Node.js and more.
-          </p>
+        <div className="tc">
+          <h1 className="weather-title">Weather</h1>
+          <SearchBox searchChange={onSearchChange} />
+          <CardList robots={filteredRobots} />
         </div>
       </div>
     </div>
