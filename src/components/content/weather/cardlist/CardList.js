be7eb9d5bd8c8d02cf757653c1a8d3dad936899d
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-31 22:38:07 +0200

    changed css for card list added grid and background

--- src/components/content/weather/cardlist/CardList.js
+++ src/components/content/weather/cardlist/CardList.js
@@ -4,17 +4,22 @@ import "./CardList.css";
 
 const CardList = ({ days }) => {
   return (
-    <div className="card-list">
-      {days.map((user, ind) => {
-        return (
-          <Card
-            key={ind}
-            id={days[ind].id}
-            name={days[ind].name}
-            description={days[ind].description}
-          />
-        );
-      })}
+    <div className="card-list shadow-5 white pa4">
+      <h1 className="card-list-title">Next 5 days Forecasts </h1>
+      <div className="card-list-grid">
+        {days.map((user, ind) => {
+          return (
+            <Card
+              key={ind}
+              id={days[ind].id}
+              name={days[ind].name}
+              tempMin={days[ind].tempMin}
+              tempMax={days[ind].tempMax}
+              image={days[ind].image}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
