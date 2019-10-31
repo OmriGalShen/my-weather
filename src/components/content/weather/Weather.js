83969bc90f5d7a4818f3b53e4477114e1e0012fc
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-31 17:33:04 +0200

    working on getting info from weather api

--- src/components/content/weather/Weather.js
+++ src/components/content/weather/Weather.js
@@ -2,24 +2,74 @@ import React, { useState, useEffect } from "react";
 import "./Weather.css";
 import SearchBox from "./searcbox/SearchBox";
 import CardList from "./cardlist/CardList";
+import CityWeather from "./CityWeather/CityWeather";
+import uuid4 from "uuid/v4";
+
+const API_KEY = "oAANJ78p2PzQFD4707OrKAwj5DPuPbQZ";
+const DEFAULT_CITY_KEY = "215854";
+
+const dayList = [
+  { id: 1, name: "sunday", tempMin: "1", tempMax: "1" },
+  { id: 1, name: "sunday", tempMin: "1", tempMax: "1" },
+  { id: 1, name: "sunday", tempMin: "1", tempMax: "1" }
+];
 
 const Weather = () => {
   const [searchfield, setSearchfield] = useState("");
-  const [robots, setRobots] = useState([]);
+  const [cityKey, setCityKey] = useState(DEFAULT_CITY_KEY);
+  const [days, setDays] = useState(dayList);
+  const [cityData, setCityData] = useState([]);
+  const [isMetric, setIsMetric] = useState(true);
+  const [forecasts, setForecasts] = useState([]);
+
+  async function getCurrentWeather() {
+    const res = await fetch(
+      `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}.json?apikey=${API_KEY}`
+    );
+    res
+      .json()
+      .then(res => {
+        setCityData(res[0]);
+      })
+      .catch(err => console.log);
+  }
+  async function getDailyForecasts() {
+    const res = await fetch(
+      `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}.json?apikey=${API_KEY}`
+    );
+    res
+      .json()
+      .then(res => {
+        setForecasts(res);
+      })
+      .catch(err => console.log);
+  }
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(respone => respone.json())
-      .then(users => setRobots(users))
-      .catch(console.log);
-  }, []);
+    getCurrentWeather();
+    getDailyForecasts();
+  }, [cityKey]);
 
   const onSearchChange = event => {
     setSearchfield(event.target.value);
   };
 
-  const filteredRobots = robots.filter(robot => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-  });
+  const fiveDayForecasts = () => {
+    if (forecasts) {
+      console.log(forecasts.DailyForecasts);
+      console.log("hey");
+      let myList = [];
+      for (let day in forecasts.DailyForecasts) {
+        console.log("hey");
+        days.push({
+          id: uuid4(),
+          name: day.Date,
+          temp: day.Temperature.Minimum.Value
+        });
+      }
+      setDays(myList);
+    }
+  };
 
   return (
     <div className="weather">
@@ -27,7 +77,8 @@ const Weather = () => {
         <div className="tc">
           <h1 className="weather-title">Weather</h1>
           <SearchBox searchChange={onSearchChange} />
-          <CardList robots={filteredRobots} />
+          <CityWeather data={cityData} isMetric={isMetric} />
+          <CardList days={days} />
         </div>
       </div>
     </div>
