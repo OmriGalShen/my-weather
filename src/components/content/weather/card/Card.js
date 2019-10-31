a926d40fdf00fa566f6fe60179824be3f555a895
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-31 22:39:19 +0200

    changed style and info displayed by card

--- src/components/content/weather/card/Card.js
+++ src/components/content/weather/card/Card.js
@@ -1,14 +1,17 @@
 import React from "react";
+import "./Card.css";
 
 const Card = props => {
-  const { name, description, id } = props;
+  const { name, tempMin, tempMax, image } = props;
 
   return (
-    <div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?200x200`} alt="profil pic" />
+    <div className="card bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={image} alt="profil pic" />
       <div>
         <h2>{name}</h2>
-        <p>{description}</p>
+        <p>
+          {tempMin}°C-{tempMax}°C
+        </p>
       </div>
     </div>
   );
