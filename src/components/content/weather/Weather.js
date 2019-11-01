ad6a42ed99164ac6e3d5f46b3c51a9c3c7d6f621
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-01 23:05:13 +0200

    lifted isMetric to app component and changed names

--- src/components/content/weather/Weather.js
+++ src/components/content/weather/Weather.js
@@ -3,31 +3,32 @@ import "./Weather.css";
 import SearchBox from "./searcbox/SearchBox";
 import CardList from "./cardlist/CardList";
 import CityWeather from "./CityWeather/CityWeather";
-import { getCurrentWeather, getDailyForecasts, getCityInfo } from "./fetchData";
+import { setCurrentWeather, setDailyForecasts, setCityInfo } from "./fetchData";
 import { Grid } from "@material-ui/core";
 
-const API_KEY = "HAlqYHjCEwSEWWecyKx02GmgGoLEavIG";
-const DEFAULT_CITY_KEY = "215854";
+const API_KEY = "HAlqYHjCEwSEWWecyKx02GmgGoLEavIG"; //need to be hidden on production
+const DEFAULT_CITY_KEY = "215854"; // tel aviv default city
 
-const Weather = () => {
+const Weather = ({ isMetric }) => {
   const [searchfield, setSearchfield] = useState("");
+  const [days, setDays] = useState([]); //list of 5 days of daily Forecasts
   const [cityKey, setCityKey] = useState(DEFAULT_CITY_KEY);
-  const [days, setDays] = useState([]);
-  const [cityData, setCityData] = useState([]);
-  const [cityName, setCityName] = useState("Tel-Aviv");
+  const [cityForecast, setcityForecast] = useState([]);
+  const [cityName, setCityName] = useState("Tel Aviv");
   const [countryName, setCountryName] = useState("Israel");
-  const [isMetric, setIsMetric] = useState(true);
 
+  //called only at the start of the app
   useEffect(() => {
-    getCurrentWeather(API_KEY, setCityData, cityKey);
-    getDailyForecasts(API_KEY, setDays, cityKey);
-  }, []);
+    setCurrentWeather(API_KEY, setcityForecast, cityKey);
+    setDailyForecasts(API_KEY, setDays, cityKey);
+  }, [cityKey]);
 
+  //user submit new city
   const handleSearchSubmit = e => {
     e.preventDefault();
-    getCityInfo(API_KEY, searchfield, setCityKey, setCityName, setCountryName);
-    getCurrentWeather(API_KEY, setCityData, cityKey);
-    getDailyForecasts(API_KEY, setDays, cityKey);
+    setCityInfo(API_KEY, searchfield, setCityKey, setCityName, setCountryName);
+    setCurrentWeather(API_KEY, setcityForecast, cityKey);
+    setDailyForecasts(API_KEY, setDays, cityKey);
   };
 
   const onSearchChange = event => {
@@ -38,7 +39,6 @@ const Weather = () => {
     <div className="weather">
       <div className="weather-panel shadow-5">
         <Grid container>
-          {/* <div className="header shadow-5"> */}
           <Grid item xs={12} className="header shadow-5">
             <Grid item xs={12}>
               <div className="weather-title">
@@ -54,14 +54,14 @@ const Weather = () => {
           </Grid>
           <Grid item lg={3} xs={12}>
             <CityWeather
-              data={cityData}
+              data={cityForecast}
               cityName={cityName}
               countryName={countryName}
               isMetric={isMetric}
             />
           </Grid>
           <Grid item lg={9} xs={12}>
-            <CardList days={days} />
+            <CardList days={days} isMetric={isMetric} />
           </Grid>
         </Grid>
       </div>
