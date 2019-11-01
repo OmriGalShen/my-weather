c06f31e8142227f034f84d37439d6c3c0a1c38bb
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-02 01:33:44 +0200

    refactoring

--- src/components/content/weather/cardlist/CardList.js
+++ src/components/content/weather/cardlist/CardList.js
@@ -7,15 +7,15 @@ const CardList = ({ days, isMetric }) => {
     <div className="card-list shadow-5 white pa4">
       <h1 className="card-list-title">Next 5 days Forecasts </h1>
       <div className="card-list-grid">
-        {days.map((user, ind) => {
+        {days.map((day, ind) => {
           return (
             <Card
               key={ind}
-              id={days[ind].id}
-              name={days[ind].name}
-              tempMin={days[ind].tempMin}
-              tempMax={days[ind].tempMax}
-              image={days[ind].image}
+              id={day.id}
+              name={day.name}
+              tempMin={day.tempMin}
+              tempMax={day.tempMax}
+              image={day.image}
               isMetric={isMetric}
             />
           );
