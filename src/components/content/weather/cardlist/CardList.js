f227117b5c51f3496229e1c349072238779d40ec
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-01 23:06:38 +0200

    added isMetric prop

--- src/components/content/weather/cardlist/CardList.js
+++ src/components/content/weather/cardlist/CardList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "../card/Card.js";
 import "./CardList.css";
 
-const CardList = ({ days }) => {
+const CardList = ({ days, isMetric }) => {
   return (
     <div className="card-list shadow-5 white pa4">
       <h1 className="card-list-title">Next 5 days Forecasts </h1>
@@ -16,6 +16,7 @@ const CardList = ({ days }) => {
               tempMin={days[ind].tempMin}
               tempMax={days[ind].tempMax}
               image={days[ind].image}
+              isMetric={isMetric}
             />
           );
         })}
