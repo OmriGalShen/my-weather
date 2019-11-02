7ec014e746e72a600f3e248f058eb523644c33a2
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-02 20:37:35 +0200

    created a list of favorite cities

--- src/components/content/favorites/Favorites.js
+++ src/components/content/favorites/Favorites.js
@@ -1,40 +1,57 @@
 import React from "react";
 import "./Favorites.css";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  IconButton
+} from "@material-ui/core";
+import LocationCityIcon from "@material-ui/icons/LocationCity";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-const Favorites = () => {
+const Favorites = ({
+  favCities,
+  setFavCities,
+  hundleCityChoose,
+  cityKey,
+  favoritesRemove
+}) => {
   return (
-    <div className="favorites">
+    <div className="favorites shaodw-5">
       <div className="favorites-panel shadow-5">
         <h1 className="favorites-title">Favorites</h1>
-        <div>
-          <p>
-            I was first exposed to programming at 8th grade when my friend
-            showed me a c++ program he wrote, which seemed very cool to me.
-            Together we wrote very simple text games, using tutorials in
-            Youtube.
-          </p>
-          <p>
-            In 10th grade, I choose to study computer science and physics. In
-            the computer science class, we learned the basics of programming in
-            C#. I also made a simple news site using HTML/CSS/JS and C# with
-            .NET framework. I also participated in a project called "Computer
-            Science, Academia and Industry" by Weizmann Institute of Science, in
-            which I made a simple geometric optics simulator (WinForms
-            application) using C#.
-          </p>
-          <p>
-            In the middle of my mandatory army service, I decided I miss
-            studying and I was motivated to develop myself. To so I started to
-            take Computer Science courses at the Open University. I studies
-            fully independently using mostly books and some videos at the
-            University site.
-          </p>
-          <p>
-            Today I study independently second-year courses but interested in my
-            first job in the field. In the meanwhile, I also study using the
-            internet Python, React, Node.js and more.
-          </p>
-        </div>
+        <p className="favorites-description">
+          View and manage your favorite cities!
+        </p>
+        <List component="nav" aria-label="favorite cities">
+          {favCities.map((favCity, index) => {
+            let itemText = favCity.country + ", " + favCity.name;
+            return (
+              <ListItem
+                button
+                key={index}
+                onClick={() => hundleCityChoose(favCity)}
+                selected={favCity.key === cityKey}
+              >
+                <ListItemIcon>
+                  <LocationCityIcon style={{ color: "white" }} />
+                </ListItemIcon>
+                <ListItemText primary={itemText} style={{ color: "white" }} />
+
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() =>
+                    favoritesRemove(favCity, favCities, setFavCities)
+                  }
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </ListItem>
+            );
+          })}
+        </List>
       </div>
     </div>
   );
