43a7e9001ced52ff5d391b42d87214c873b5dd62
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-02 02:36:07 +0200

    fixed footer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Grid from "@material-ui/core/Grid";
+// import Grid from "@material-ui/core/Grid";
 import Weather from "../content/weather/Weather";
 import Favorites from "../content/favorites/Favorites";
 import Settings from "../content/settings/Settings";
@@ -19,41 +19,35 @@ const App = () => {
 
   return (
     <div className="App">
-      <Grid container>
-        <Router>
-          <Grid item xs={12}>
-            <div className="navbar">
-              <Navbar linksList={linksList} />
-            </div>
-          </Grid>
-          <Grid item xs={12}>
-            <div className="main-container">
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => <Weather {...props} isMetric={isMetric} />}
-                />
-                <Route
-                  exact
-                  path="/favorites"
-                  render={props => <Favorites {...props} isMetric={isMetric} />}
-                />
-                <Route
-                  exact
-                  path="/settings"
-                  render={props => (
-                    <Settings {...props} setIsMetric={setIsMetric} />
-                  )}
-                />
-              </Switch>
-            </div>
-          </Grid>
-          <Grid item xs={12}>
-            <Footer />
-          </Grid>
-        </Router>
-      </Grid>
+      <Router>
+        <div className="navbar">
+          <Navbar linksList={linksList} />
+        </div>
+        <div className="main-container">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => <Weather {...props} isMetric={isMetric} />}
+            />
+            <Route
+              exact
+              path="/favorites"
+              render={props => <Favorites {...props} isMetric={isMetric} />}
+            />
+            <Route
+              exact
+              path="/settings"
+              render={props => (
+                <Settings {...props} setIsMetric={setIsMetric} />
+              )}
+            />
+          </Switch>
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
+      </Router>
     </div>
   );
 };
--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,28 +1,13 @@
 import React from "react";
 import "./Footer.css";
-import Paper from "@material-ui/core/Paper";
-import { makeStyles } from "@material-ui/core/styles";
-
-//In order to change style of material ui compontents this jsx styling needs to be used
-const useStyles = makeStyles(theme => ({
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: "center",
-    background: "#3f51b5",
-    color: "white"
-  }
-}));
 
 const Footer = () => {
-  const classes = useStyles();
   return (
     <div className="shadow-5 footer white ">
-      <Paper className={classes.paper}>
-        <div className="footer-text">
-          <p className="f6">Made By Omri Gal Shenhav ©</p>
-          <p className="f6">2019</p>
-        </div>
-      </Paper>
+      <div className="footer-text">
+        <p className="f6">Made By Omri Gal Shenhav ©</p>
+        <p className="f6">2019</p>
+      </div>
     </div>
   );
 };
