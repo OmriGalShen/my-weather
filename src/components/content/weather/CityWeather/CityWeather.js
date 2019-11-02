d0cfe32f7fadfd5f315b9a0bc1cf7342f5abc315
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-02 04:46:37 +0200

    added favorite icon

--- src/components/content/weather/CityWeather/CityWeather.js
+++ src/components/content/weather/CityWeather/CityWeather.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./CityWeather.css";
 import { getWeatherImage } from "../../../weatherImages/weatherImages";
+import { FormControlLabel, Checkbox } from "@material-ui/core";
+import { FavoriteBorder, Favorite } from "@material-ui/icons";
 
-const CityWeather = ({ data, isMetric, city }) => {
+const FavoriteButton = ({ checked, handleFavorite }) => {
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          style={{ display: "flex", justifyContent: "center" }}
+          icon={<FavoriteBorder fontSize="large" />}
+          checkedIcon={<Favorite fontSize="large" />}
+          value={true}
+          checked={checked}
+          onChange={handleFavorite}
+        />
+      }
+      label=""
+      labelPlacement="top"
+    />
+  );
+};
+
+const CityWeather = ({ data, isMetric, city, handleFavorite }) => {
   const unit = isMetric ? "°C" : "°F";
   let information = "";
   if (data.Temperature) {
@@ -13,6 +34,7 @@ const CityWeather = ({ data, isMetric, city }) => {
       <div>
         <h3 className="city-name-title">{city.country},</h3>
         <h1 className="city-name-title">{city.name}</h1>
+
         <h2>{data.WeatherText}</h2>
         <img alt="weather" src={getWeatherImage(1)} />
         <h2>
@@ -22,9 +44,17 @@ const CityWeather = ({ data, isMetric, city }) => {
     );
   }
   return (
-    <div className="city-weather shadow-5 white">
-      <h1>Now</h1>
-      <div className="city-weather-info">{information}</div>
+    <div className="city-weather-wrapper">
+      <div className="tc pa1">
+        <FavoriteButton
+          checked={city.isFavorite}
+          handleFavorite={handleFavorite}
+        />
+      </div>
+      <div className="city-weather white">
+        <h1>Now</h1>
+        <div className="city-weather-info">{information}</div>
+      </div>
     </div>
   );
 };
