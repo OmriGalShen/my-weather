177ab4c0f87c82cd5997ae84bcaaa4882f9b0706
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-03 23:01:18 +0200

    redesign search button

--- src/components/content/weather/searcbox/SearchBox.js
+++ src/components/content/weather/searcbox/SearchBox.js
@@ -3,28 +3,6 @@ import "./SearchBox.css";
 import { Button } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles({
-  notchedOutline: {
-    borderWidth: "1px",
-    borderColor: "white"
-  },
-  input: {
-    height: "3em",
-    color: "white"
-  },
-  button: {
-    background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(33, 203, 243, .3)",
-    color: "white",
-    fontWeight: "bold",
-    padding: "0.8em",
-    margin: 5
-  }
-});
 
 const SearchBox = props => {
   const {
@@ -33,7 +11,6 @@ const SearchBox = props => {
     searchfield,
     filteredCities
   } = props;
-  const classes = useStyles();
   return (
     <form className="searchbox" onSubmit={handleSearchSubmit} method="post">
       <Autocomplete
@@ -47,19 +24,13 @@ const SearchBox = props => {
             {...params}
             placeholder="Search city"
             fullWidth
-            variant="outlined"
+            variant="filled"
             value={searchfield}
             onChange={onSearchChange}
-            InputProps={{
-              classes: {
-                notchedOutline: classes.notchedOutline,
-                input: classes.input
-              }
-            }}
           />
         )}
       />
-      <Button className={classes.button} type="submit" size="medium">
+      <Button type="submit" className="submit" size="medium">
         search
       </Button>
     </form>
