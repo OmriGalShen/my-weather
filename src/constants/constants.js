64570915aa89a7a4d9eb5b7fb0ddee1c8eb8505d
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-05 20:59:52 +0200

    clean code

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,19 @@
+/* This site uses the AccuWeather RESTful API at 'https://developer.accuweather.com/'  */
+
+export const API_KEY = "dQDM7rn5uDuuIU9TMsyHAFasqPqAtSkW"; //need to be hidden on production
+
+// api urls
+export const AUTOCOMPLETE_URL =
+  "https://dataservice.accuweather.com/locations/v1/cities/autocomplete";
+export const CURRENT_CONDITION_URL =
+  "https://dataservice.accuweather.com/currentconditions/v1/";
+export const FIVE_DAILY_URL =
+  "https://dataservice.accuweather.com/forecasts/v1/daily/5day/";
+export const GEOPOSITION_SEARCH_URL =
+  "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
+export const CITY_NEIGHBORS_URL =
+  "https://dataservice.accuweather.com/locations/v1/cities/neighbors/";
+
 // default city - tel aviv
 export const DEFAULT_CITY = {
   id: "215854",
@@ -16,19 +32,3 @@ export const DEFAULT_FAV_CITIES = [
     isFavorite: true
   }
 ];
-
-/* API - This site uses the AccuWeather api at 'https://developer.accuweather.com/' */
-
-export const API_KEY = "XSVcKEvy5e1Yba8ZY8AasZiKxJ56Aigb"; //need to be hidden on production
-
-// api urls
-export const AUTOCOMPLETE_URL =
-  "https://dataservice.accuweather.com/locations/v1/cities/autocomplete";
-export const CURRENT_CONDITION_URL =
-  "https://dataservice.accuweather.com/currentconditions/v1/";
-export const FIVE_DAILY_URL =
-  "https://dataservice.accuweather.com/forecasts/v1/daily/5day/";
-export const GEOPOSITION_SEARCH_URL =
-  "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
-export const CITY_NEIGHBORS_URL =
-  "https://dataservice.accuweather.com/locations/v1/cities/neighbors/";
