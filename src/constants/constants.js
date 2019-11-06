7e70a4debbd9fdcad5fd0f137844fdad8079e147
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-11-06 10:48:22 +0200

    updated api key

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 /* This site uses the AccuWeather RESTful API at 'https://developer.accuweather.com/'  */
 
-export const API_KEY = "8c3cYDbAGoiOFNphcChay8FAoPHGnu1B"; //need to be hidden on production
+export const API_KEY = "BRQL2OIyE7PqI2F2dIBAmileU0N58UFy"; //need to be hidden on production
 
 // api urls
 export const AUTOCOMPLETE_URL =
